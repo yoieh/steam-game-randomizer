5bfca3775b0bb4935730aa40bb20366da47ecb08
yoieh <yoieh@live.se>
2023-08-15 01:16:59 +0200

    implemented 1 hour cache of games for each user

--- src/app/api/user/[slug]/games/route.ts
+++ src/app/api/user/[slug]/games/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
+let cache = new Map();
+let cacheTime = 60 * 60 * 1000; // 1 hour
+let lastCacheRelease = Date.now();
+
 export async function GET(
   request: Request,
   {
@@ -13,6 +17,17 @@ export async function GET(
 
   const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${slug}&format=json&include_appinfo=true`;
   try {
+    // Clear the cache if it's been more than 1 hour
+    if (Date.now() - lastCacheRelease > cacheTime) {
+      cache = new Map();
+      lastCacheRelease = Date.now();
+    }
+
+    // Check if we have a cached response
+    if (cache.has(slug)) {
+      return NextResponse.json(cache.get(slug));
+    }
+
     const response = await fetch(url);
     if (!response.ok) {
       const statusText = response.statusText;
@@ -21,6 +36,10 @@ export async function GET(
       );
     }
     const data = await response.json();
+
+    // Cache the response for 1 hour
+    cache.set(slug, data.response.games);
+
     return NextResponse.json(data.response.games);
   } catch (error) {
     console.error("Error fetching game library:", error);
