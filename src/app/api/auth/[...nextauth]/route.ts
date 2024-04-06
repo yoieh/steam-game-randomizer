3dc722bd63d3c02cc73e55e79d9f56e0d93b15cc
yoieh <yoieh@live.se>
2024-04-06 15:01:18 +0200

    correct handling of auth routes bump patche of next-auth and next-auth-steam added peer  dependency "openid-client@^5.4.2"

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,17 @@
 import { getAuthOptions } from "@/app/auth";
-import type { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth/next";
+import { NextRequest } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, getAuthOptions(req));
+export async function GET(
+  req: NextRequest,
+  ctx: { params: { nextauth: string[] } }
+) {
+  return NextAuth(req, ctx, getAuthOptions(req));
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, getAuthOptions(req));
+export async function POST(
+  req: NextRequest,
+  ctx: { params: { nextauth: string[] } }
+) {
+  return NextAuth(req, ctx, getAuthOptions(req));
 }
