04885927e2b075fc46a689a29ffd41f0a2399da1
yoieh <yoieh@live.se>
2024-04-07 04:21:47 +0200

    add infinity scroll to game list

--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -1,19 +1,60 @@
 "use client";
 
-import { GameCard, GameCardSkeleton } from "@/components/GameCard";
+import { GameCard } from "@/components/GameCard";
+import { GameCardSkeleton } from "./GameCardSkeleton";
 import { useGames } from "@/hooks/useGames";
+import { useEffect, useMemo, useState } from "react";
+
+const NEXT_AMOUNT = 8;
 
 export const GamesList = () => {
   const { filteredGames } = useGames();
+  const [visibleGames, setVisibleGames] = useState(
+    filteredGames.slice(0, NEXT_AMOUNT)
+  );
+  const [loading, setLoading] = useState(false);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    setLoading(true);
+    setTimeout(() => {
+      setVisibleGames((prevGames) => [
+        ...prevGames,
+        ...filteredGames.slice(
+          prevGames.length,
+          prevGames.length + NEXT_AMOUNT
+        ),
+      ]);
+      setLoading(false);
+    }, 200); // Simulate network delay
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  if (!filteredGames.length) {
+    return (
+      <div className="flex flex-wrap justify-center gap-4 mb-8 sm:w-full">
+        {[...Array(NEXT_AMOUNT)].map((_, i) => (
+          <GameCardSkeleton key={i} />
+        ))}
+      </div>
+    );
+  }
 
   return (
-    <div className="flex flex-wrap justify-center mb-8 sm:w-full gap-4">
-      {!filteredGames.length &&
-        [...Array(10)].map((_, i) => <GameCardSkeleton key={i} />)}
-
-      {filteredGames.map((game) => (
+    <div className="flex flex-wrap justify-center gap-4 mb-NEXT_AMOUNT sm:w-full">
+      {visibleGames.map((game) => (
         <GameCard key={game.appid} game={game} />
       ))}
+      {loading &&
+        [...Array(NEXT_AMOUNT)].map((_, i) => <GameCardSkeleton key={i} />)}
     </div>
   );
 };
--- src/components/RandomGame.tsx
+++ src/components/RandomGame.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import React from "react";
-import { GameCard, GameCardSkeleton } from "./GameCard";
+import { GameCard } from "./GameCard";
+import { GameCardSkeleton } from "./GameCardSkeleton";
 import { useRandomGame } from "@/hooks/useRandomGame";
 
 export const RandomGame = () => {
